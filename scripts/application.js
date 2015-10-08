fbf305b6f2801c138e63e3b8b6b676929e5db0bd
SakeWolf <nocturne.wolf@hotmail.com>
2015-10-08 13:29:18 -0700

    Refactored javascript to be a bit cleaner and more functional.

--- scripts/application.js
+++ scripts/application.js
@@ -1,18 +1,16 @@
 $(document).ready(function () {
   var metronome;
   var metronomeOn = false;
+  var currentBpm = 0;
 
   var sound = document.createElement("audio");
   sound.setAttribute("src", "http://www.flashkit.com/imagesvr_ce/flashkit/soundfx/Instruments/Drums/Claves/idg_Clav-intermed-1390/idg_Clav-intermed-1390_hifi.mp3");
 
   $("#play").click(function () {
     var bpm = $("#bpm").val();
+    metronomeOn = resetIfNewBpm(bpm);
     if (!metronomeOn && bpm > 0) {
-      var interval = 60 / bpm;
-      sound.play();
-      metronome = setInterval(function() {
-        sound.play();
-      }, Math.floor(1000 * interval));
+      metronome = startMetronome(bpm, sound);
       metronomeOn = true;
     }
   });
@@ -21,4 +19,21 @@ $(document).ready(function () {
     clearInterval(metronome);
     metronomeOn = false;
   });
+
+  function startMetronome(bpm, sound) {
+    var interval = 60 / bpm;
+    sound.play();
+    return setInterval(function() {
+      sound.play();
+    }, Math.floor(1000 * interval));
+  }
+
+  function resetIfNewBpm (newBpm) {
+    if (newBpm != currentBpm) {
+      clearInterval(metronome);
+      return false;
+    } else {
+      return true;
+    }
+  }
 });
